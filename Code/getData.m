de905ce8bec49fde949b48c252c6d89796d8d779
Warda Syeda <wsyeda@student.unimelb.edu.au>
2016-11-17 09:52:04 +1100

    fifth commit

--- Code/getData.m
+++ Code/getData.m
@@ -1,25 +1,36 @@
-function getData(imgName)
+function getData(imgName)  % Telling MATLAB this file is a function
 
+% Identifying image for data extraction
 switch imgName
     
     case 'Batman.jpg'
+        
+        % Loading image
         im = imread(imgName);
+        
+        % Displaying image
         figure; imagesc(im); axis off
         
+        % Requesting user to mark data axis + data points
         [x,y] = getpts(gcf); y = -y;
         
+        % Identifying first three points as axis points
         x_O = x(1); y_O = y(1);
         x_L = x(2); y_L = y(3);
         
+        % Converting pixel positions to data
         x_data = (x(4:end)-x_O) * (40-0)./(x_L - x_O) + 0;
         y_data = (y(4:end)-y_O) * (100-0)./(y_L - y_O) + 0;
         
+        % Plotting Data
         figure; plot(x_data,y_data)
         
+        % Labelling data plot
         xlabel('Velocity profile of Batman glide')
         ylabel ('Velocity')
         title ('Trajectory of a falling Batman')
         
+        % Setting axis limits
         set(gca, 'xlim',[0 40]);
         set(gca, 'ylim',[0,130]);
         
